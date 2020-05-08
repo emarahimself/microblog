ec375547039d0313efa508b5411c0f17bc5bbabb
emarahimself <m.a.emara@live.com>
2020-05-08 04:49:33 +0200

    Added SQL modules and confiurations

--- src/config.py
+++ src/config.py
@@ -1,4 +1,9 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24).hex()
+    DATABASE_URL = os.environ.get('DATABASE_URL') or \
+                    r'sqlite://' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
