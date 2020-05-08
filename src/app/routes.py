2c0804edc719b15ab36179e4f7c81753196123c9
emarahimself <m.a.emara@live.com>
2020-05-08 03:29:28 +0200

    Added login form via flask-wtf

--- src/app/routes.py
+++ src/app/routes.py
@@ -1,5 +1,7 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, flash
 from app import app
+from app.forms import LoginForm
+
 
 @app.route('/')
 @app.route('/index')
@@ -8,3 +10,14 @@ def index():
         'username': 'Mohamed'
     }
     return render_template('index.html', title='Home', user=user)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        flash(f'Login requested for user: {form.username.data}')
+        return redirect(url_for('index'))
+
+    return render_template('login.html', form=form, title='Sign In')
--- src/config.py
+++ src/config.py
@@ -1,4 +1,4 @@
 import os
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or str(os.uname(24))
+    SECRET_KEY = os.environ.get('SECRET_KEY') or str(os.urandom(24))
